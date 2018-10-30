ed16f8f50d55d75042b0d82e52b5309931152b2b
kongxx3282 <kongxx3282@gmail.com>
2018-10-30 14:26:32 +0800

    update

--- myapp/admin.py
+++ myapp/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
-
+from .models import MyUser, Version, Favorite, PictureVersion, Picture, Download
 # Register your models here.
+
+admin.site.register(MyUser)
+admin.site.register(Version)
+admin.site.register(Favorite)
+admin.site.register(PictureVersion)
+admin.site.register(Picture)
+admin.site.register(Download)
