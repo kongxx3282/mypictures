a399d840ee677c792758988d9841377e076244b0
lulucici <yolandachen7@zju.edu.cn>
2018-11-12 19:46:24 +0800

    frontend

--- mypictures/settings.py
+++ mypictures/settings.py
@@ -16,6 +16,11 @@ import os
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 
+STATIC_URL = '/static/'
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static/'),
+)
+
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/1.11/howto/deployment/checklist/
 
